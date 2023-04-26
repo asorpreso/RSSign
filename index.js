87c81a83d2f79a470f7c2cddb45a3ac8119d3428
Andre Sorpreso <asorpreso@uol.com.br>
2023-04-26 17:05:20 -0300

    inclusao dos arquivos salvos

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+function showPDF() {
+  window.open("https://rssign.netlify.app/documento.pdf", "_blank");
+}
+async function generateHash() {
+  // recupera o arquivo PDF do servidor
+  const pdfResponse = await fetch("https://www.example.com/document.pdf");
+  const pdfBuffer = await pdfResponse.arrayBuffer();
+
+  // recupera os 5 dígitos do CPF informados pelo usuário
+  const cpf = document.getElementById("cpf").value.slice(0, 5);
+
+  // concatena o PDF com o CPF e gera o hash SHA256
+  const pdfCpfBuffer = new TextEncoder().encode(`${cpf}${pdfBuffer}`);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", pdfCpfBuffer);
+
+  // converte o hash para uma string hexadecimal
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  // exibe o hash na tela
+  document.getElementById("hash").textContent = hashHex;
+}
